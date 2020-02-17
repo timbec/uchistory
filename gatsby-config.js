3770994db900baaa753511380ed8f4fb3eb74c43
Tim Beckett <timbeckett@Tims-Air.home>
2020-02-17 14:31:56 -0500

    Updated README.

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-source-graphql`, 
+      options: {
+        typeName: `WPGraphql`, 
+        fieldName: `wpgraphql`,
+        url: `http://localhost:8888/uraniumcity-history/graphql`
+      }
+    }, 
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
